a0460f55f03d7c061db36848710a99f170809f6b
“John <ma4197@cumc.columbia.edu>
2022-09-18 22:11:36 -0400

    Finish of Problem 2

--- p8105_hw1_ma4197.R
+++ p8105_hw1_ma4197.R
@@ -51,3 +51,19 @@ library(tidyverse)
 for them, as obsevred with icecream due to it having a value between 0 and 1. However,
 due to the other variables being character variables with no value assigned to them, a mean
 value cannot be generated.`
+
+## Numeric Function
+as.numeric(pull(icecream_df, icecream_logical))
+as.numeric(pull(icecream_df, icecream_char))
+as.numeric(pull(icecream_df, icecream_factor))
+
+`When using the numeric function for each of the variables, it is evident why a mean
+value could not be taken. For the logical vector, the values outputted were 0 and 1 
+signifying the true and false response generated, however a mean value cannot be taken 
+of these responses as it is not possible to have a middle value for true or false in
+the frame. For the character vector, all responses were NA due to the character nature 
+thus with no numeric value they cannot have a generated mean. Lastly, for the factor
+vector, the output was the levels from 1 to 3, however similar to the logic vectors, there
+cannot be a mean response of the three levels created, as each of these level values
+are also arbitrarily assigned values. As a result a mean cannot be taken for these variables
+due to the returned values being uninterpretable in the context of the dataframe.`
